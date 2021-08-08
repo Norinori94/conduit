b7d3217cdbfe04204f3405f03e5dfe7b4550c1ac
Tamas Nora <tamasnora94@gmail.com>
2021-08-08 15:37:09 +0200

    add test files2

--- test/conduit_app_methods.py
+++ test/conduit_app_methods.py
@@ -1,6 +1,8 @@
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
+import time
+
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 
 # Felületi elem azonosítása
@@ -27,12 +29,15 @@ def login(browser, email, password):
     input_login_email.send_keys(email)
     input_login_password.send_keys(password)
     button_sign_in.click()
+    time.sleep(1)
 
 
+# Navbar elemek azonosítása
 def navbar_elements(browser):
     return wait_for_elements(browser, By.XPATH, '//ul[@class="nav navbar-nav pull-xs-right"]/li')
 
 
+# Bejegyzés létrehozása/módosítása
 def edit_article(browser, article_title, article_about, article_content):
     wait_for_element(browser, By.XPATH, '//input[@placeholder="Article Title"]').clear()
     input_article_title = wait_for_element(browser, By.XPATH, '//input[@placeholder="Article Title"]')
@@ -43,24 +48,19 @@ def edit_article(browser, article_title, article_about, article_content):
     input_article_about.send_keys(article_about)
 
     wait_for_element(browser, By.XPATH, '//textarea[@placeholder="Write your article (in markdown)"]').clear()
-    input_article_content = wait_for_element(browser, By.XPATH, '//textarea[@placeholder="Write your article (in markdown)"]')
+    input_article_content = wait_for_element(browser, By.XPATH,
+                                             '//textarea[@placeholder="Write your article (in markdown)"]')
     input_article_content.send_keys(article_content)
 
     button_publish_article = wait_for_element(browser, By.XPATH, '//button[@type="submit"]')
     button_publish_article.click()
 
-# def edit_article(browser, article_title, article_about, article_content):
-#     input_article_title = wait_for_element(browser, By.XPATH, '//input[@placeholder="Article Title"]')
-#     input_article_title.clear()
-#     input_article_title.send_keys(article_title)
-#
-#     input_article_about = wait_for_element(browser, By.XPATH, '//input[@placeholder="What\'s this article about?"]')
-#     article_about.clear()
-#     input_article_about.send_keys(article_about)
-#
-#     input_article_content = wait_for_element(browser, By.XPATH, '//textarea[@placeholder="Write your article (in markdown)"]')
-#     article_content.clear()
-#     input_article_content.send_keys(article_content)
-#
-#     button_publish_article = wait_for_element(browser, By.XPATH, '//button[@type="submit"]')
-#     button_publish_article.click()
+
+# A felhasználó bejegyzéseinek (lista) megnyitása
+def open_user_articles(browser):
+    time.sleep(2)
+    navbar_username = wait_for_element(browser, By.XPATH, '//a[@href="#/@teszt001/"]')
+    navbar_username.click()
+    time.sleep(0.5)
+    browser.refresh()
+    time.sleep(1) # 2
--- test/test_conduit_app.py
+++ test/test_conduit_app.py
@@ -1,4 +1,4 @@
-import time
+import csv
 
 from selenium import webdriver
 from conduit_app_methods import *
@@ -6,6 +6,7 @@ from conduit_app_methods import *
 
 class TestConduitApp(object):
 
+    # Böngésző indítása
     def setup(self):
         self.browser = webdriver.Chrome("C://Windows//chromedriver.exe")
         self.browser.maximize_window()
@@ -15,6 +16,7 @@ class TestConduitApp(object):
         self.email = "teszt001@email.hu"
         self.password = "Teszt001"
 
+    # Böngésző bezárása
     def teardown(self):
         self.browser.quit()
 
@@ -49,10 +51,12 @@ class TestConduitApp(object):
         input_registration_username.send_keys(self.username)
         input_registration_email.send_keys(self.email)
         input_registration_password.send_keys(self.password)
+        time.sleep(0.5)
         button_sign_up.click()
 
         div_title = wait_for_element(self.browser, By.XPATH, '//div[@class="swal-title"]')
         div_text = wait_for_element(self.browser, By.XPATH, '//div[@class="swal-text"]')
+        time.sleep(0.5)
         button_registration_ok = wait_for_element(self.browser, By.XPATH, '//button[@class="swal-button '
                                                                           'swal-button--confirm"]')
 
@@ -82,9 +86,8 @@ class TestConduitApp(object):
         assert self.username in navbar_elements_text_list
 
     # Új adat bevitel teszt
-    def test_post_new_article(self):
+    def test_create_new_article(self):
         login(self.browser, self.email, self.password)
-        time.sleep(1)
         navbar_new_article = wait_for_element(self.browser, By.XPATH, '//a[@href="#/editor"]')
         navbar_new_article.click()
 
@@ -105,16 +108,13 @@ class TestConduitApp(object):
     # Meglévő adat módosítás teszt
     def test_edit_article(self):
         login(self.browser, self.email, self.password)
-        time.sleep(1)
-        navbar_username = wait_for_element(self.browser, By.XPATH, '//a[@href="#/@teszt001/"]')
-        navbar_username.click()
-        time.sleep(1)
 
-        article_list = wait_for_elements(self.browser, By.XPATH, '//a[@href="#/articles/title-of-the-article--test"]')
+        open_user_articles(self.browser)
+
+        article_list = wait_for_elements(self.browser, By.XPATH, '//a[@class="preview-link"]')
         article_list[0].click()
 
-        button_edit_article = wait_for_element(self.browser, By.XPATH, '//a[@href="#/editor/title-of-the-article'
-                                                                       '--test"]')
+        button_edit_article = wait_for_element(self.browser, By.XPATH, '//span[contains(text(), "Edit")]/..')
         button_edit_article.click()
 
         article_title = "Title of the article - Test2 - Edited"
@@ -128,6 +128,32 @@ class TestConduitApp(object):
 
         assert new_article_title.text == article_title
         assert new_article_content.text == article_content
+
+    # Ismételt és sorozatos adatbevitel adatforrásból
+    def test_create_new_article_from_csv(self):
+        login(self.browser, self.email, self.password)
+
+        with open("articles.csv", "r", encoding="utf-8") as articles_csv:
+            next(articles_csv)
+            articles = csv.reader(articles_csv, delimiter=",")
+
+            for article in articles:
+                navbar_new_article = wait_for_element(self.browser, By.XPATH, '//a[@href="#/editor"]')
+                navbar_new_article.click()
+                time.sleep(1)
+
+                article_title = article[0]
+                article_about = article[1]
+                article_content = article[2]
+                edit_article(self.browser, article_title, article_about, article_content)
+
+                new_article_title = wait_for_element(self.browser, By.XPATH, '//h1')
+                new_article_content = wait_for_element(self.browser, By.XPATH, '//div[@class="col-xs-12"]/div/p')
+                author_of_article = wait_for_element(self.browser, By.XPATH, '//a[@class="author"]')
+
+                assert new_article_title.text == article_title
+                assert new_article_content.text == article_content
+                assert author_of_article.text == self.username
 
     # Több oldalas lista bejárása teszt
     def test_navigate_in_pages(self):
@@ -144,10 +170,116 @@ class TestConduitApp(object):
 
         assert number_of_page == len(pages)
 
+    # Adatok listázása
+    def test_list_user_articles(self):
+        login(self.browser, self.email, self.password)
+        open_user_articles(self.browser)
+
+        author_of_articles_list = set()
+        author_of_articles = wait_for_elements(self.browser, By.XPATH, '//div[@class="article-preview"]/div/div/a')
+
+        if len(self.browser.find_elements_by_xpath('//a[@class="page-link"]')) != 0:
+
+            pages = wait_for_elements(self.browser, By.XPATH, '//a[@class="page-link"]')
+            number_of_page = 1
+
+            for p in pages[1:]:
+                for author in author_of_articles:
+                    author_of_articles_list.add(author.text)
+                p.click()
+                number_of_page += 1
+        else:
+            for author in author_of_articles:
+                author_of_articles_list.add(author.text)
+
+        assert len(author_of_articles_list) == 1
+        assert self.username in author_of_articles_list
+
+    # Adatok lementése felületről
+    def test_export_articles(self):
+        login(self.browser, self.email, self.password)
+
+        navbar_home = wait_for_element(self.browser, By.XPATH, '//a[@href="#/"]')
+        navbar_home.click()
+        time.sleep(1)
+
+        header = ['Author', 'Article Title', 'What\'s this article about?']
+        rows = []
+        number_of_articles_at_home_page = 0
+        number_of_articles_in_csv = 0
+
+        if len(self.browser.find_elements_by_xpath('//a[@class="page-link"]')) != 0:
+
+            pages = wait_for_elements(self.browser, By.XPATH, '//a[@class="page-link"]')
+            number_of_page = 0
+
+            for p in pages[0:]:
+                p.click()
+                number_of_page += 1
+                time.sleep(1)
+
+                author_of_articles = wait_for_elements(self.browser, By.XPATH, '//a[@class="author"]')
+                article_title = wait_for_elements(self.browser, By.XPATH, '//a[@class="preview-link"]/h1')
+                article_about = wait_for_elements(self.browser, By.XPATH, '//a[@class="preview-link"]/p')
+
+                for index in range(len(author_of_articles)):
+                    row = []
+                    row.append(author_of_articles[index].text)
+                    row.append(article_title[index].text)
+                    row.append(article_about[index].text)
+                    rows.append(row)
+                    number_of_articles_at_home_page += 1
+        else:
+            author_of_articles = wait_for_elements(self.browser, By.XPATH, '//a[@class="author"]')
+            article_title = wait_for_elements(self.browser, By.XPATH, '//a[@class="preview-link"]/h1')
+            article_about = wait_for_elements(self.browser, By.XPATH, '//a[@class="preview-link"]/p')
+
+            for index in range(len(author_of_articles)):
+                row = []
+                row.append(author_of_articles[index].text)
+                row.append(article_title[index].text)
+                row.append(article_about[index].text)
+                rows.append(row)
+                number_of_articles_at_home_page += 1
+
+        with open("exported_articles.csv", "w", newline="", encoding="utf-8") as articles_csv:
+            writer = csv.writer(articles_csv)
+            writer.writerow(header)
+            for row in rows:
+                writer.writerow(row)
+
+        with open("exported_articles.csv", "r", encoding="utf-8") as articles_csv:
+            next(articles_csv)
+            articles = csv.reader(articles_csv, delimiter=",")
+
+            for article in articles:
+                number_of_articles_in_csv += 1
+
+        assert number_of_articles_at_home_page == number_of_articles_in_csv
+
+    # Adat vagy adatok törlése
+    def test_delete_article(self):
+        login(self.browser, self.email, self.password)
+        user_article_exist = True
+
+        while user_article_exist:
+            open_user_articles(self.browser)
+            first_article = wait_for_elements(self.browser, By.XPATH, '//div[@class="article-preview"]')[0]
+
+            if first_article.text != "No articles are here... yet.":
+                article_list = wait_for_elements(self.browser, By.XPATH, '//a[@class="preview-link"]')
+                article_list[0].click()
+                button_delete_article = wait_for_element(self.browser, By.XPATH, '//span[contains(text(), "Delete")]/..')
+                button_delete_article.click()
+            else:
+                user_article_exist = False
+
+        first_article = wait_for_elements(self.browser, By.XPATH, '//div[@class="article-preview"]')[0]
+        assert first_article.text == "No articles are here... yet."
+
     # Kijelentkezés teszt
     def test_logout(self):
         login(self.browser, self.email, self.password)
-        time.sleep(1)
         number_of_navbar_elements_before_log_out = len(navbar_elements(self.browser))
         navbar_log_out = wait_for_element(self.browser, By.XPATH, '//a[@active-class="active"]')
         navbar_log_out.click()
